ff298df340b4075d6b33eb1d0e84ed8887c1dcb7
Andrew Haralambous <143616104+aharalam@users.noreply.github.com>
2026-09-17 14:17:12 -0400

    Update checkout.py
    Change checkout total calculation (incorrect for lab purposes).

--- checkout.py
+++ checkout.py
@@ -2,4 +2,4 @@ cart = []
 
 def get_total():
     # Calculate the total price of items in the cart:
-    return sum(item['price'] * item['quantity'] for item in cart)
+    return sum(item['price'] + item['quantity'] for item in cart)
