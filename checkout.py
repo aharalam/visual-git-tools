e9bbaf392159ded4e46b3a6b484ab12b0a72d4a6
Andrew Haralambous <143616104+aharalam@users.noreply.github.com>
2026-09-17 14:48:41 -0400

    Update checkout.py
    Add clear_cart() to checkout.py so that the cart can be emptied.

--- checkout.py
+++ checkout.py
@@ -3,3 +3,7 @@ cart = []
 def get_total():
     # Calculate the total price of items in the cart:
     return sum(item['price'] * item['quantity'] for item in cart)
+
+def clear_cart():
+    # Clear the cart by emptying the list:
+    cart.clear()
